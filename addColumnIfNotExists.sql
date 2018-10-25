cd9ab4bc75dae2f4192ed95374100e1aeea2f67e
wangjunjie <wangjunjie@winteam500.com>
2018-10-25 22:28:33 +0800

    feat: addColumnIfNotExists

---
+++ addColumnIfNotExists.sql
@@ -0,0 +1,19 @@
+DROP PROCEDURE IF EXISTS "addColumnIfNotExists";
+CREATE PROCEDURE "addColumnIfNotExists"(
+	IN tableName tinytext,
+	IN fieldName tinytext,
+	IN fieldDef text)
+BEGIN
+	IF NOT EXISTS (
+		SELECT * FROM information_schema.COLUMNS
+		WHERE column_name=fieldName
+		AND table_name=tableName
+		AND table_schema=schema()
+		)
+	THEN
+		SET @sql=CONCAT('ALTER TABLE ', schema(), '.', tableName,
+			' ADD COLUMN ', fieldName, ' ', fieldDef);
+		PREPARE stmt from @sql;
+		EXECUTE stmt;
+	END IF;
+END
